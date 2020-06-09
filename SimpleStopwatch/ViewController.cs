fd6b9c753e67db375a78525186a9c72274d9f2a8
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2020-06-09 15:04:01 -0400

    Initial Commit

---
+++ SimpleStopwatch/ViewController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using AppKit;
+using Foundation;
+
+namespace SimpleStopwatch
+{
+    public partial class ViewController : NSViewController
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            Time.StringValue = _stopwatch.Elapsed.ToString("g");
+
+            var timer = NSTimer.CreateRepeatingTimer(TimeSpan.FromMilliseconds(100), t =>
+            {
+                if (_stopwatch.IsRunning)
+                {
+                    Time.StringValue = _stopwatch.Elapsed.ToString("g");
+                }
+            });
+            
+            NSRunLoop.Main.AddTimer(timer, NSRunLoopMode.Common);
+        }
+
+        partial void Reset(AppKit.NSButton sender)
+        {
+            if (_stopwatch.IsRunning)
+            {
+                _stopwatch.Restart();
+            }
+            else
+            {
+                _stopwatch.Reset();
+            }
+
+            Time.StringValue = _stopwatch.Elapsed.ToString("g");
+        }
+
+        partial void Start(AppKit.NSButton sender)
+        {
+            if (_stopwatch.IsRunning)
+            {
+                _stopwatch.Stop();
+                sender.Title = "Start";
+            }
+            else
+            {
+                _stopwatch.Start();
+                sender.Title = "Stop";
+            }
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
---
+++ SimpleStopwatch/ViewController.designer.cs
@@ -0,0 +1,34 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Rider IDE
+//   to store outlets and actions made in Xcode.
+// If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace SimpleStopwatch
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSTextField Time { get; set; }
+
+		[Action ("Reset:")]
+		partial void Reset (AppKit.NSButton sender);
+
+		[Action ("Start:")]
+		partial void Start (AppKit.NSButton sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (Time != null) {
+				Time.Dispose ();
+				Time = null;
+			}
+
+		}
+	}
+}
